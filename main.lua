d398ac566ce0900bb59b694a4c9e12eaf01f127e
Johan Lidberg <johan.lidberg@skola.botkyrka.se>
2021-03-15 16:34:10 +0100

    senaste

--- main.lua
+++ main.lua
@@ -1,13 +1,21 @@
 
 
 function love.load()
+
   world = love.physics.newWorld(0, 400)
 
   ball = {}
   ball.body = love.physics.newBody(world, 400, 200, "dynamic")
   ball.body:setMass(5)
-  ball.shape = love.physics.newPolygonShape(100, 100, 200, 100, 200, 200)
+  ball.shape = love.physics.newPolygonShape(0, 0, 0, 20, 40, 20, 40, 0)
   ball.fixture = love.physics.newFixture(ball.body, ball.shape)
+
+  ball2 = {}
+  ball2.body = love.physics.newBody(world, 400, 200, "dynamic")
+  ball2.body:setMass(5)
+  ball2.shape = love.physics.newPolygonShape(0, 0, 0, 20, 40, 20, 40, 0)
+  ball2.fixture = love.physics.newFixture(ball2.body, ball2.shape)
+
 
   block = {}
   block.body = love.physics.newBody(world, 200, 450, 'static')
@@ -16,7 +24,7 @@ function love.load()
 
   block.sbody = love.physics.newBody(world, 200, 200, 'static')
   block.fshape = love.physics.newPolygonShape(0, 0, 0, 20, 400, 20, 400, 0)
-  block.gfixture = love.physics.newFixture(block.body, block.shape)
+  block.gfixture = love.physics.newFixture(block.sbody, block.fshape)
 
 
 end
@@ -30,22 +38,36 @@ function love.update(dt)
     ball.body:applyForce(-100, 0)
   end
   if love.keyboard.isDown("w") then
-    ball.body:applyForce(100, -1000)
+    ball.body:applyForce(0, -1000)
   elseif love.keyboard.isDown("s") then
     ball.body:applyForce(0, 1000)
 end
-end
 
+if love.keyboard.isDown("right") then
+  ball2.body:applyForce(100, 0)
+elseif love.keyboard.isDown("left") then
+  ball2.body:applyForce(-100, 0)
+end
+if love.keyboard.isDown("up") then
+  ball2.body:applyForce(0, -1000)
+elseif love.keyboard.isDown("down") then
+  ball2.body:applyForce(0, 1000)
+end
+end
 
 function love.draw()
   love.graphics.setBackgroundColor{0, 0, 1, 1}
 
+  love.graphics.setColor(0, 1, 0, 1)
   love.graphics.polygon('fill', ball.body:getWorldPoints(ball.shape:getPoints()))
 
-  love.graphics.setColor{0, 1, 0, 1}
-  love.graphics.polygon('fill', block.sbody:getWorldPoints(block.fshape:getPoints()))
+  love.graphics.setColor(0, 1, 0, 1)
+  love.graphics.polygon('fill', ball2.body:getWorldPoints(ball2.shape:getPoints()))
 
-
+  love.graphics.setColor{0, 1, 1, 1}
   love.graphics.polygon('fill', block.body:getWorldPoints(block.shape:getPoints()))
-  love.graphics.setColor{0, 1, 0, 1}
+
+  love.graphics.setColor{0, 1, 1, 1}
+  love.graphics.polygon('fill', block.sbody:getWorldPoints(block.fshape:getPoints()))
+  
 end
